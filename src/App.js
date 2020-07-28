c7a3489776815dcc16d8e75f730b6965a1ef88e0
Izabella Assad <izabellaassad@gmail.com>
2020-07-28 02:03:49 -0300

    Mudanças

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+
       />
 
       <Carousel
@@ -27,23 +27,16 @@ function App() {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />      
+      />
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
 
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
 
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
+
+
 
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
-import Button from '../Button';
+import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+import Button from '../Button'
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
       <a href="/">
-        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+
+        {/* <img className="Logo" src={Logo} alt="AluraFlix logo" /> */}
       </a>
 
       <Button as="a" className="ButtonLink" href="/">
         Novo vídeo
       </Button>
     </nav>
-  );
+  )
 }
 
-export default Menu;
+export default Menu
