0d6c05cc9a04bac47bfc27fe5c1b9c03142fbca5
Izabella Assad <izabellaassad@gmail.com>
2020-07-30 15:46:44 -0300

    mudanças no carousel

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,12 +6,22 @@ function CadastroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+            <form>
+                <label>
+                    Nome da Categoria:
+          <input
+                        type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+        </button>
+            </form>
             <Link to="/">
                 Ir para Home
             </Link>
-        </PageDefault>
-
-
+        </PageDefault >
     )
 }
 
