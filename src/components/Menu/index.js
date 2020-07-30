39a5b45904abcb249a01c8f0d042eaaa7e42ca63
Izabella Assad <izabellaassad@gmail.com>
2020-07-30 15:22:58 -0300

    rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './Menu.css'
 import Button from '../Button'
+import { Link } from 'react-router-dom'
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
-
+      <Link to="/">
         <img className="Logo" src="https://fontmeme.com/permalink/200729/ba1cad500473f37b7abe91ec9525b356.png" alt="AluraFlix logo" />
-      </a>
-
-      <Button as="a" className="ButtonLink" href="/">
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video" >
         Novo vídeo
       </Button>
     </nav>
