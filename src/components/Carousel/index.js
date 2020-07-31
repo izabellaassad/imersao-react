0252b828bbc14308786d60612b740c860e9e8b33
Izabella Assad <izabellaassad@gmail.com>
2020-07-30 21:17:37 -0300

    Formulario

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles'
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles'
 import Slider, { SliderItem } from './components/Slider'
 import VideoCard from './components/VideoCard'
 
