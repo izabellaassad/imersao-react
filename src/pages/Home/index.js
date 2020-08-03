a4d0ee134bb9f9d245a7e594b969df43f2f41ce7
Izabella Assad <izabellaassad@gmail.com>
2020-08-03 19:33:29 -0300

    test bd

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,43 +1,29 @@
-import React from 'react'
-import Menu from '../../components/Menu'
-import dadosIniciais from '../../data/dados_iniciais.json'
-import BannerMain from '../../components/BannerMain'
-import Carousel from '../../components/Carousel'
-import Footer from '../../components/Footer'
-
+import React from 'react';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
-    <div style={{ background: "#141414" }}>
+    <div style={{ background: '#141414' }}>
       <Menu />
 
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-
       />
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+      <Carousel category={dadosIniciais.categorias[1]} />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-
-
-
-
-
+      <Carousel category={dadosIniciais.categorias[2]} />
 
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
